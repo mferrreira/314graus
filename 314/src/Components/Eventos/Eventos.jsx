688e49770bf5f8212e7bb9b3abbe038d52f1dc22
mferrreira <masterplays02@gmail.com>
2023-04-20 08:41:38 -0300

    dev/front updated and new backend development

--- 314/src/Components/Eventos/Eventos.jsx
+++ 314/src/Components/Eventos/Eventos.jsx
@@ -1,75 +1,67 @@
-import { React, useState, useRef, useEffect } from "react"
-import './Eventos.css'
-import axios from 'axios'
-
-const data = [
-    {
-        id: 1,
-        name: "Item 1",
-        description: "Description of item 1",
-    },
-    {
-        id: 2,
-        name: "Item 2",
-        description: "Description of item 2",
-    },
-    {
-        id: 3,
-        name: "Item 3",
-        description: "Description of item 3",
-    },
-];
-
+import { React, useState, useRef, useEffect } from "react";
+import './Eventos.css';
+import axios from 'axios';
 
 export default function Eventos() {
-    const [selectedItem, setSelectedItem] = useState(null);
-    const ref = useRef(null);
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [eventos, setEventos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const ref = useRef(null);
 
-    const handleClick = (index) => {
-        setSelectedItem(index);
-    };
+  useEffect(()=> {
+    axios.get('http://localhost:4000/eventos')
+    .then(res => {
+        console.log(res)
+        setEventos(res.data)
+        setLoading(false);
+    })
+    .catch(e => console.log(e))
+  }, [])
 
-    const handleOutsideClick = (event) => {
-        if (ref.current && !ref.current.contains(event.target)) {
-            setSelectedItem(null);
-        }
-    };
+  const handleClick = (index) => {
+      setSelectedItem(index);
+  };
 
-    useEffect(() => {
-        document.addEventListener("mousedown", handleOutsideClick);
-        return () => {
-            document.removeEventListener("mousedown", handleOutsideClick);
-        };
-    }, []);
+  const handleOutsideClick = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+          setSelectedItem(null);
+      }
+  };
 
-    return (
-        <div id="wrapper" className="text-black">
-            <h1 className="text-center text-7xl">Garanta já seu ingresso</h1>
-            <div className="relative pt-10" ref={ref}>
-                <div className="flex justify-center">
-                    {data.map((item, index) => (
-                        <div
-                            key={item.id}
-                            className="w-52 h-52 rounded-full bg-gray-300 cursor-pointer mx-2 my-8"
-                            onClick={() => handleClick(index)}
-                        ></div>
-                    ))}
-                </div>
-                {selectedItem !== null && (
-                    <div className="flex justify-center content-center mt-10">
-                        <div
-                            className=" w-72 h-72 bg-white shadow-lg rounded-lg p-4 ">
-                            <h2 className="font-bold text-lg">{data[selectedItem].name}</h2>
-                            <p className="text-gray-600">{data[selectedItem].description}</p>
-                        </div>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  useEffect(() => {
+      document.addEventListener("mousedown", handleOutsideClick);
+      return () => {
+          document.removeEventListener("mousedown", handleOutsideClick);
+      };
+  }, []);
+
+  if (loading) {
+    return <div className="text-center text-lg">carregando...</div>;
+  }
+
+  return (
+      <div id="wrapper" className="text-black">
+          <h1 className="text-center text-7xl">Compre agora</h1>
+          <div className="relative pt-10" ref={ref}>
+              <div className="flex justify-center">
+                  {eventos.map((item, index) => (
+                      <div key={item.id} className="w-52 h-52 rounded-full bg-gray-300 cursor-pointer mx-2 my-8" onClick={() => handleClick(index)}>
+                          <div className="flex justify-center items-center h-full">
+                              <p>{item.name}</p>
+                          </div>       
+                      </div>
+                  ))}
+              </div>
+              {selectedItem !== null && (
+                  <div className="flex justify-center content-center mt-10">
+                      <div
+                          className=" w-[50vw] h-96 bg-white shadow-lg rounded-lg p-4 ">
+                          <h2 className="font-bold text-lg text-center">{eventos[selectedItem].name}</h2>
+                          <p className="text-gray-600">{eventos[selectedItem].description}</p>
+                      </div>
+                  </div>
+              )}
+          </div>
+      </div>
+  );
 };
-
-
-
-
-
